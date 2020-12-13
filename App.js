5fa6060a3b070d8451e2e8e7e1a4bdb17c25dff1
alestor123 <alestoraldous@gmail.com>
2020-12-13 22:09:30 +0530

    added comment

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-var chalk = require('chalk');
+const { settings } = require('cluster');
 
+var chalk = require('chalk'),
+options = JSON,
+fs = require('fs');
+
+// log
 var loggerjs = (message) => {
-    console.log(chalk.green('(LOG): ' + Date() + ': '  + message))
+    var log = chalk.green('(LOG): ' + Date() + ': '  + message)
+    console.log(log)
 }
-
+// info
 loggerjs.info = (message) => {
-    console.info(chalk.blue('(INFO): ' + Date() + ': '  + message))
+    var info = chalk.blue('(INFO): ' + Date() + ': '  + message)
+    console.info(info)
+    fsLog(info)
 }
+// warn
 loggerjs.warn = (message) => {
-    console.warn(chalk.yellow('(WARN): ' + Date() + ': '  + message))
+var warn = chalk.yellow('(WARN): ' + Date() + ': '  + message)
+console.warn(warn)
+fsLog(warn)
 }
+// error
 loggerjs.error = (message) => {
-    console.error(chalk.red('(ERROR): ' + Date() + ': '  + message))
+var error = chalk.red('(ERROR): ' + Date() + ': '  + message);
+console.error(error)
+fsLog(error)
 }
+// emergency
 loggerjs.emergency = (message) => {
-    console.error(chalk.bgYellow.red('(EMERGENCY): ' + Date() + ': '  + message))
+var emergency = chalk.bgYellow.red('(EMERGENCY): ' + Date() + ': '  + message);
+console.error(emergency)
+fsLog(emergency)
+}
+// settings
+loggerjs.set = (option) => {
+options = option
+loggerMsg('OPTIONS FOUND')
+}
+// main logging for the logger 
+function loggerMsg(message){
+    console.log(chalk.bgWhite.green('(LOGGER) : ' + Date() + ': ' + message))
+}
+// file logging 
+function fsLog(logText) {
+if(options.fsLog || false){
+fs.appendFile(options.fileName||'logs.log',`\n ${logText} \n` , (err) => {
+    if (err) throw err;
+  });
+    }
 }
 module.exports = loggerjs
--- run.js
+++ run.js
@@ -1,5 +1,7 @@
-var loggerjs = require('./App');
+var loggerjs = require('./App'),
+fs = require('fs')
 
+loggerjs.set({fsLog:true,fileName:'pro.log'})
 loggerjs.info('Info')
 loggerjs.warn('Yeah')
 loggerjs.error('Yeah')
